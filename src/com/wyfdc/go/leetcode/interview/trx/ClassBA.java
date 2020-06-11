8d7daa0a50d96ac68ca1de7af4da7e67befa8597
sanwancoder <1250772810@qq.com>
2020-06-11 11:46:41 +0800

    feat:ClassBA

---
+++ src/com/wyfdc/go/leetcode/interview/trx/ClassBA.java
@@ -0,0 +1,29 @@
+package com.wyfdc.go.leetcode.interview.trx;
+
+class A{
+    static {
+        System.out.print("1");
+    }
+    public A(){
+        System.out.print("2");
+    }
+}
+
+class B extends A{
+    static {
+        System.out.print("a");
+    }
+    public B(){
+        System.out.print("b");
+    }
+}
+
+public class ClassBA {
+
+    // 1a2b2b
+    public static void main(String[] args) {
+        A ab = new B();
+        ab = new B();
+    }
+
+}
