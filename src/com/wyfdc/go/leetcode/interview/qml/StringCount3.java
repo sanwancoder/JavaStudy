0b8a88c33c918c8f0cf8139ca087b8b9c8dd40b8
sanwancoder <1250772810@qq.com>
2020-10-06 20:28:57 +0800

    add onlien judge

--- src/com/wyfdc/go/leetcode/interview/qml/StringCount3.java
+++ src/com/wyfdc/go/leetcode/interview/qml/StringCount3.java
@@ -2,6 +2,12 @@ package com.wyfdc.go.leetcode.interview.qml;
 
 import java.util.Objects;
 
+/**
+ * 1. 有一串字符串String s = "ababab",
+ * 这个字符串可以看做由3个"ab"构成,即n=3, L = "ab", s = nL.
+ * 现在要求编写一段程序,使用单例模式,输入任意字符串s,输出nL.
+ * 如输入: aaaaa 输出 5a ,输入: ababa 输出: 1ababa
+ */
 public class StringCount3 {
 
     private static StringCount3 tool = new StringCount3();
